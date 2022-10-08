bcb984ac4be8b8e8e8ff28d16902c3dc33d24798
Rick Selby <rick@selby-family.co.uk>
2022-10-08 09:43:28 +0100

    Update docblock for getServiceProviderClass

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -7,13 +7,6 @@ use RickSelby\Laravel\Auth\ExternalBasicAuthServiceProvider;
 
 abstract class AbstractTestCase extends AbstractPackageTestCase
 {
-    /**
-     * Get the service provider class.
-     *
-     * @param \Illuminate\Contracts\Foundation\Application $app
-     *
-     * @return string
-     */
     protected function getServiceProviderClass()
     {
         return ExternalBasicAuthServiceProvider::class;
